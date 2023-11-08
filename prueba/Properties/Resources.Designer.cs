865cdf0e3637dc2f485fcc42e6e3091fa9f1ab5f
Julio Adrian Ferreyra <jaft210403@outlook.com>
2023-11-07 21:32:02 -0600

    Cambie la interfaz EditarCitaForm

--- prueba/Properties/Resources.Designer.cs
+++ prueba/Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace CapaPresentacion.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap lupa {
+            get {
+                object obj = ResourceManager.GetObject("lupa", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PatitaLogo {
             get {
                 object obj = ResourceManager.GetObject("PatitaLogo", resourceCulture);
